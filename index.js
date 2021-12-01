4aefbd4a2045498f28f7e93b4bd73f849662bfa3
AadityaKhurrana <90464753+AadityaKhurrana@users.noreply.github.com>
2021-12-01 16:34:20 +0530

    All Done

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ var txt=document.getElementById("txtarea");
 var output=document.querySelector("#output");
 console.log(txt);
 
-var data=txt.value;
+
+//var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// now mking a banana app so we take api url
+var url="https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(inputText)
+{
+    return url+"?"+"text="+inputText;
+}
+function errorHandler(error)
+{
+    console.log("error occured",error);
+}
 
 btn_trans.addEventListener('click',function print(){
     console.log("Button clicked");
-    output.innerText="Your text is "+txt.value;
+    var data=txt.value;
+    //output.innerText="Your text is "+txt.value;
     //console.log(txt.value);
     //console.log(txt.innerText);
+
+    //fetch url
+    fetch(constructUrl(data))
+    .then(response=>response.json())
+    .then(json=>{
+        var tranlate_text=json.contents.translated;
+        output.innerText=tranlate_text;
+    });
+
 
 })
